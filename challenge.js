76a0c5089011f1ef8ac90660412859d138705c8f
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-15 13:37:21 -0600

    added video solution.

--- challenge.js
+++ challenge.js
@@ -48,3 +48,20 @@ johnBmi = johnMass / johnHeight ** 2;
 console.log("Data 2:", markBmi, johnBmi)
 console.log(markBmi > johnBmi)
 
+/// Solution ///
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn =1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn =1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI); 
